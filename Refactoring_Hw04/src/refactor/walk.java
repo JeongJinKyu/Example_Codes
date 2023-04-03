a1780608b6fbd5e86477f44a7650f124870f3151
JeongJinKyu <104819739+JeongJinKyu@users.noreply.github.com>
2023-04-03 18:20:39 +0900

    Update walk.java
    getinstance	구현

--- Refactoring_Hw04/src/refactor/walk.java
+++ Refactoring_Hw04/src/refactor/walk.java
@@ -1,6 +1,9 @@
 package src.refactor;
 
 public class walk implements move{
+	private static Walk instance = new Walk():
+	private Walk(){}
+	public static getinstance(){return instance;}
     public void Get_move(){
         System.out.println("I can only walk.");
     }
