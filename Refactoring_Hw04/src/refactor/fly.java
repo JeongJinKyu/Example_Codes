eb3c03ca4144d3a83ece29869510f2383e25d0af
JeongJinKyu <104819739+JeongJinKyu@users.noreply.github.com>
2023-04-03 13:27:35 +0900

    Update fly.java
    싱글톤 패턴 적용

--- Refactoring_Hw04/src/refactor/fly.java
+++ Refactoring_Hw04/src/refactor/fly.java
@@ -1,6 +1,10 @@
 package src.refactor;
 
 public class fly implements move{
+
+	private static Fly instance = new Fly();
+	private Fly() {	}
+	public static Fly getinstance(){return instance;}
     public void Get_move(){
         System.out.println("I can fly.");
     }
