29a30661a9e340ea0e80d99662f2a1d8f871d7d1
JeongJinKyu <104819739+JeongJinKyu@users.noreply.github.com>
2023-04-03 13:11:31 +0900

    Update RobotMain.java
    싱글톤 패턴 적용 && strategy pattern 이 적용 안된(context가 concrete strategy 에 대한 의존성 가지는 부분) 부분 수정

--- Refactoring_Hw04/src/refactor/RobotMain.java
+++ Refactoring_Hw04/src/refactor/RobotMain.java
@@ -4,9 +4,22 @@ package src.refactor;
 public class RobotMain {
     public static void main(String[] args) {
         Robot[] robots = new Robot[3];
-        robots[0] = new TaekwonV("TaekwonV");
-        robots[1] = new Atom("Atom");
-        robots[2] = new Sungard("Sungard");
+
+		/**
+		싱글톤 패턴을 이용하여 메모리 낭비 방지
+		**/
+		PunchAttack punch_attack = PunchAttack.getinstance();
+		RocketAttack rocket_attack = RocketAttack.getinstance();
+		Fly fly_ = Fly.getinstance();
+		Walk walk_ = Walk.getinstance();
+
+		/**
+		strategy pattern 에서 context는 concrete strategy를 "몰라야" 함
+		즉, 의존성을 가지면 안됨
+		**/
+        robots[0] = new TaekwonV("TaekwonV", punch_attack, walk_);
+        robots[1] = new Atom("Atom", rocket_attack, fly_);
+        robots[2] = new Sungard("Sungard", punch_attak, fly_);
 
 
         for (Robot robot : robots) {
@@ -16,7 +29,15 @@ public class RobotMain {
         }
 
         System.out.println("after change fly to move");
+		/**
+		같은 클래스에 대하여 반복적으로 publice 하게되어있는 new 생성자를 이용하면
+		이전에 생성된 객체는 garbage 객체가 되어 메모리 낭비가 되므로
+		
+		생성자를 private하게 설정 하여 마음대로 객체 생성을 못하도록 방지 후
+		싱긆톤 패턴을 이용하여 getinstance()로만 객체를 가져올 수 있게끔 함 
+		**/
         robots[2].setMove(new walk());
+		robots[2].setAttack(punch_attack);
         robots[2].attack();
         robots[2].move();
     }
