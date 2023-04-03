b8a26a0622bceb6338e75cc99eb1d24409f90d6d
JeongJinKyu <104819739+JeongJinKyu@users.noreply.github.com>
2023-04-03 13:15:08 +0900

    Update Atom.java
    의존성 가지는 부분 수정

--- Refactoring_Hw04/src/refactor/Atom.java
+++ Refactoring_Hw04/src/refactor/Atom.java
@@ -1,7 +1,12 @@
 package src.refactor;
 
 public class Atom extends Robot {
-	public Atom(String name) {
-		super(name,new PunchAttack(),new fly() );
+/**
+context는 concrete strategy를 "몰라야" 함
+즉, 의존성을 가지면 안됨
+직접 new 생성자를 이용하면 "아는"경우가 되므로 의존성을 가지게 됨
+**/
+	public Atom(String name, Attack attack, Move move) {
+		super(name,attack,move);
 	}
 }
