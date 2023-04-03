c364827e81f2bd0e07962dcd66c262eb5471a393
JeongJinKyu <104819739+JeongJinKyu@users.noreply.github.com>
2023-04-03 13:25:22 +0900

    Update Sungard.java
    dependency	없애기

--- Refactoring_Hw04/src/refactor/Sungard.java
+++ Refactoring_Hw04/src/refactor/Sungard.java
@@ -1,7 +1,7 @@
 package src.refactor;
 
 public class Sungard extends Robot {
-    public Sungard(String name) {
-        super(name,new MissileAttack(),new fly());
+    public Sungard(String name,Attack, attack, Move move) {
+        super(name,attack,move);
     }
 }
