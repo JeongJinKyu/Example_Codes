73359e7aade3a83f888849c9efe209e03e046b75
JeongJinKyu <104819739+JeongJinKyu@users.noreply.github.com>
2023-04-03 18:18:07 +0900

    Update MissileAttack.java
    getinstance 구현

--- Refactoring_Hw04/src/refactor/MissileAttack.java
+++ Refactoring_Hw04/src/refactor/MissileAttack.java
@@ -1,6 +1,9 @@
 package src.refactor;
 
 public class MissileAttack implements attack{
+	private static MissileAttack instance = new MissileAttack();
+	private MissileAttack(){}
+	public static MissileAttack getinstance(){return instance;}
     public void Do_Attack(){
         System.out.println("I have Missile and can attack with it.");
     }
